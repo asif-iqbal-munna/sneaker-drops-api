b9ff461a8d03e1a10bf290f6df19c12273827643
Munna <asifiqbalmunna1318@gmail.com>
2026-02-11 01:54:30 +0600

    api added

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ app.get('/api/health', (_req: Request, res: Response) => {
   res.json({ status: 'ok' });
 });
 
-app.get('/', (_req: Request, res: Response) => {
+app.get('/api', (_req: Request, res: Response) => {
   res.json({ message: 'sneaker-drops-api up and running' });
 });
 
